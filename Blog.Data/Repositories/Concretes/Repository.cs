965b233f091b9cfed3be4cbaabac5436333943e7
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-11-28 01:58:01 +0300

    28.11.2022 Irepository interface classlart eklendi

---
+++ Blog.Data/Repositories/Concretes/Repository.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Core.Entities;
+using Blog.Data.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Repositories.Concretes
+{
+    public class Repository<T>:IRepository<T> where T : class,IEntityBase,new()
+    {
+        private readonly DbContext _dbContext;
+
+        public Repository(DbContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+        }
+        private DbSet<T>Table { get => _dbContext.Set<T>(); }
+        public async Task AddAsync(T entity)
+        {// Task ile void aynı işlevi görüyor o yüzden bir şe ydönmesine gerek yoktur. voidin async halidir işte 
+            await Table.AddAsync(entity);
+        }
+    }
+}
