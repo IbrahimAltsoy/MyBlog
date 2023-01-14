a6ea92e841e11d7113be752f6378beeca0f33851
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-01-14 23:56:14 +0300

    Added UserController

---
+++ Blog.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOS.Users;
+using Blog.Entity.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper mapper;
+
+        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            this._userManager = userManager;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var map = mapper.Map<List<UserDTO>>(users);
+            foreach(var item in map)
+            {
+                var findUser = await _userManager.FindByIdAsync(item.Id.ToString());
+                var role = string.Join ("",await _userManager.GetRolesAsync(findUser));
+                item.Role = role;
+            }
+            return View(map);
+        }
+    }
+}
